3bd275afa04fec4c0a371a10d969be39aab9f328
Relvees <7695747+Relvees@users.noreply.github.com>
2017-12-15 09:30:43 +0000

    Some Structure to the Index/Details
    Still looks like trash, but at least it's functional trash

--- newssite/news/views.py
+++ newssite/news/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse
 from .models import Article
 
 def index(request):
-	return render(request,'news/index.html')
+	latest_article_list = Article.objects.all()
+	return render(request,'news/index.html', {'latest_article_list': latest_article_list})
 	
 def detail(request, article_id):
 	article = get_object_or_404(Article, pk=article_id)
