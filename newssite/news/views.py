691a7a2d4d16630e69cd207a35fe85842db9cc46
Relvees <7695747+Relvees@users.noreply.github.com>
2017-12-15 07:07:36 +0000

    Added WIP templates, views
    Basic views for when I get objects to twiddle with

--- newssite/news/views.py
+++ newssite/news/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
 from django.http import HttpResponse
 
+from .models import Article
 def index(request):
-    return HttpResponse("Hello, World! You are currently visiting this news site that is currently a WIP.")
+	return render(request,'news/index.html')
+	
+def detail(request, Article_id):
+	article = get_object_or_404(Article, pk=article_id)
+	return render(request, 'news/article.html', {'article': article})
