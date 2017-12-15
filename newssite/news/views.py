a293cb3b5867a22a18e616fd871a4a6052ab9a6c
Relvees <7695747+Relvees@users.noreply.github.com>
2017-12-15 08:22:21 +0000

    Functionality for articles template
    Basic for now, fleshing out template to display attributes

--- newssite/news/views.py
+++ newssite/news/views.py
@@ -3,9 +3,10 @@ from django.http import Http404
 from django.http import HttpResponse
 
 from .models import Article
+
 def index(request):
 	return render(request,'news/index.html')
 	
-def detail(request, Article_id):
+def detail(request, article_id):
 	article = get_object_or_404(Article, pk=article_id)
-	return render(request, 'news/article.html', {'article': article})
+	return render(request, 'news/detail.html', {'article': article})
