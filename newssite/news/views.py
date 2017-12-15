fd7311ef09f74599f4cdc8946d0f5aefb9dd681c
Relvees <7695747+Relvees@users.noreply.github.com>
2017-12-15 11:55:39 +0000

    Files for the Version Control God
    Added JQuery 3.2.1 for upvote shenanegans
    -some functionality for voting
    inb4 something breaks

--- newssite/news/views.py
+++ newssite/news/views.py
@@ -11,3 +11,14 @@ def index(request):
 def detail(request, article_id):
 	article = get_object_or_404(Article, pk=article_id)
 	return render(request, 'news/detail.html', {'article': article})
+	
+def vote(request):
+	vote_type = request.POST.get('type')
+	page_id = get_object_or_404(Article, pk=article_id)
+	
+	if (vote_action == 'Like'):
+		page_id.score += 1
+	elif (vote_action == 'Dislike'):
+		page_id.score -= 1
+	else:
+		return HttpResponse('There has been an Error, please try again.')
